f981d3affce0ea3578b62ae5b52b49d730401a87
Anthony <anthony.tuffery85@orange.fr>
2021-07-29 02:05:12 +0200

    Le Baluchon Ver 0.1
    -Création LauchScreen

---
+++ Le Baluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Le Baluchon
+//
+//  Created by Anthony TUFFERY on 29/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
